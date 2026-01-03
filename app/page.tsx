e8cecd1c321d199ef3ee764dbad1f56112e1bf33
Victor Malomo <malomovictor@gmail.com>
2026-01-03 20:05:14 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+const page = () => {
+  return (
+    <section>
+      <div className="flex justify-center items-center  h-screen flex-col gap-4">
+        <h1 className="max-w-4xl ">
+          Your Clear, Simple and Accurate <br /> One‑Stop Platform for <br />{" "}
+          Nigeria’s New Tax Reforms.
+        </h1>
+
+        <p className="max-w-2xl text-center">
+          NaijaTax helps everyday Nigerians understand the new tax reforms,
+          calculate what they owe, know their rights and obligations, and stay
+          compliant with Nigerian tax laws — without jargon, fear, or guesswork.
+        </p>
+      </div>
+    </section>
+  );
+};
+export default page;
